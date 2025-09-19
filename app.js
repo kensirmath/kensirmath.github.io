b1353abc3ca13acfc357755b2b99e895d39756ef
KenSir <129938654+kensirmath@users.noreply.github.com>
2025-09-19 16:20:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -785,9 +785,9 @@ function factorQuadratic(a, b, c) {
     if (!isSimpleFraction(root1) || !isSimpleFraction(root2)) return null;
     
     // 自己加20250919.1616 (改factorization display result)
-    root1 = Math.gcd((-b + sqrtD),(2 * a))
-    root2 = Math.gcd((-b - sqrtD),(2 * a))
-    a = math.gcd(a,b,c)
+    const root1 = Math.gcd((-b + sqrtD),(2 * a))
+    const root2 = Math.gcd((-b - sqrtD),(2 * a))
+    const a = math.gcd(a,b,c)
     // 自己加20250919.1616 (改factorization display result)
 
     return formatFactorizedForm(a, root1, root2);
