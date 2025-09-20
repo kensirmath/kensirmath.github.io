cf026b167c2be2ef97355e7955302b4e4824d45a
KenSir <129938654+kensirmath@users.noreply.github.com>
2025-09-20 13:42:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -884,6 +884,8 @@ function formatFactorizedForm(a, b, c, root1, root2) {
         return `(${formatRoot2(root1)}x ${formatRoot(root1)})(${formatRoot2(root2)}x ${formatRoot(root2)})`;
     } elseif (a<0 && GCD99 === 1) {
         return `-(${formatRoot2(root1)}x ${formatRoot(root1)})(${formatRoot2(root2)}x ${formatRoot(root2)})`;
+    } elseif (a<0) {
+        return `-${GCD99}(${formatRoot2(root1)}x ${formatRoot(root1)})(${formatRoot2(root2)}x ${formatRoot(root2)})`;
     } else {
         return `${GCD99}(${formatRoot2(root1)}x ${formatRoot(root1)})(${formatRoot2(root2)}x ${formatRoot(root2)})`;
     }
